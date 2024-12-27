651f0acb7f749a4df3f82c26b773a671b734f7b5
Sasi <sasitharani@gmail.com>
2024-12-27 16:05:18 +0530

    mail chack

---
+++ server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import nodemailer from 'nodemailer';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: 'sasitharani@gmail.com',
+    pass: 'zfikzmnxyuicssim',
+  },
+});
+
+app.post('/api/send-email', (req, res) => {
+  const { name, email, phone, message } = req.body;
+
+  const mailOptions = {
+    from: 'sasitharani@gmail.com',
+    to: 'sasitharani@gmail.com', // replace with the recipient's email address
+    subject: 'Insphile-New Contact Form Submission',
+    text: `Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      return res.status(500).json({ error: 'Error sending email' });
+    }
+    console.log('Email sent:', info.response);
+    res.status(200).json({ message: 'Email sent successfully' });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
