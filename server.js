01122c22a8d72279e527f2328dc00a535a4b6305
Sasi <sasitharani@gmail.com>
2024-12-27 17:08:14 +0530

    check5

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ import cors from 'cors';
 import upload from './upload.js'; // Import the upload middleware
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3001;
