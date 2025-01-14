7dbe5123f2846a846b7529de92a847b3fc922520
Sasi <sasitharani@gmail.com>
2025-01-14 12:18:18 +0530

    original starter project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'tailwindcss/tailwind.css';
+import Header from './components/Header';
+import Front from './components/Front';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Front />
+    </>
+  );
+}
+
+export default App;
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Front from './components/Front';
-import Contact from './components/Contact';
 import Header from './components/Header';
-import Page1 from './components/Page1';
+
 
 import './App.css';
 import './tailwind.css';
@@ -13,9 +12,6 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Front />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/page1" element={<Page1 />} />
-        {/* Add other routes here */}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
