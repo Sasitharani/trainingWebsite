e3b16e7a644c15ab610e2579c5f4c66279f5c731
sasi <sasitharani@gmail.com>
2025-02-28 11:41:51 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Front from './components/Front';
 import Header from './components/Header';
-
+import CodingTraining from './components/CodingTraining';
+import PlacementTraining from './components/PlacementTraining';
 
 import './App.css';
 import './tailwind.css';
@@ -12,6 +13,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Front />} />
+        <Route path="/coding" element={<CodingTraining />} />
+        <Route path="/placement" element={<PlacementTraining />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
         <ul className="flex space-x-4">
           <li><a href="#home" className="hover:text-gray-400">Home</a></li>
           <li><a href="#about" className="hover:text-gray-400">About</a></li>
-          <li><a href="#services" className="hover:text-gray-400">Services</a></li>
+          <li><a href="#services" className="hover:text-gray-400">Courses</a></li>
           <li><a href="#contact" className="hover:text-gray-400">Contact</a></li>
         </ul>
       </nav>
